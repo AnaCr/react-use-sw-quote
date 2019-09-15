73d3f219bb50c788d27794b6e7a5e63b420973e9
Ana Cruz <ACruz@loop1.com>
2019-09-14 21:57:00 -0500

    Create custom hook

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useMyHook } from '@anacr/react-use-sw-quote'
+import React, { useState } from "react";
+import { useStarWarsQuote } from "@anacr/react-use-sw-quote";
 
 const App = () => {
-  const example = useMyHook()
-  return (
-    <div>
-      {example}
-    </div>
-  )
-}
-export default App
+  const { quote, loading } = useStarWarsQuote("");
+  console.log(loading, quote, "this is my custom hook");
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (quote) {
+    return <div>{quote}</div>;
+  }
+
+  return null;
+};
+export default App;
